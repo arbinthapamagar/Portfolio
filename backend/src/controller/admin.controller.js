cd8fdb1ab6a8a5d3373082b667468e748fb7d344
Arbeen <arbinbabuthapamagar2002@gmail.com>
2026-05-28 11:07:47 +0545

    Refactor admin authentication and token management logic; update admin model schema

--- backend/src/controller/admin.controller.js
+++ backend/src/controller/admin.controller.js
@@ -1,24 +1,170 @@
-import {Admin} from "../models/admin.model.js"
+import { Admin } from '../models/admin.model.js';
 
-import { asyncHandler } from "../utils/asyncHandler";
-import { apiError } from "../utils/apiError";
-import {apiResponse} from "../utils/apiResponse"
-import jwt from "jsonwebtoken";
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { apiError } from '../utils/apiError.js';
+import { apiResponse } from '../utils/apiResponse.js';
+import jwt from 'jsonwebtoken';
+import {
+  uploadOnCloudinary,
+  deleteFromCloudinary,
+} from '../utils/cloudinary.js';
 
 
-const generateAccessTokenAndRefreshToken = async()=>{
-try {
-        const admin =await Admin.findById(adminId);
+
+
+
+const generateAccessTokenAndRefreshToken = async (adminId) => {
+    try {
+        const admin = await Admin.findById(adminId);
         const accessToken = admin.generateAccessToken();
         const refreshtoken = admin.generateRefreshToken();
         admin.refreshToken = refreshtoken;
-        await admin.save({validateBeforeSave:false});
-        return {accessToken, refreshtoken}
-} catch (error) {
-    throw new apiError(
-        500, " Something went wrong while creating accesstoken and refresh token"
-    );
-    
-}
+        await admin.save({ validateBeforeSave: false });
+        return { accessToken, refreshtoken };
+    } catch (error) {
+        throw new apiError(
+            500,
+            ' Something went wrong while creating accesstoken and refresh token'
+        );
+    }
+};
 
-}
+const adminLogin = asyncHandler(async (req, res) => {
+    const { email, password, phoneNumber } = req.body;
+    if (!email || !password) {
+        throw new apiError(400, ' all field are required ');
+    }
+    const admin = await Admin.findOne({
+        $or: [{ email }, { phoneNumber }],
+    });
+
+    // if admin is not found say not found
+
+    if (!admin) {
+        throw new apiError(404, ' admin not found ');
+    }
+    const isPasswordCorrect = await admin.isPasswordCorrect(password);
+
+    if (!isPasswordCorrect) {
+        throw new apiError(400, ' password didnt matched ');
+    }
+
+    if (confirmPassword !== password) {
+        throw new apiError(400, ' Confirm password and password do not match. ');
+    }
+});
+
+// if all good then send the token into the cookies
+
+const { accessToken, refreshToken } = await generateAccessTokenAndRefreshToken(admin._id);
+
+const loggedAdmin = await Admin.findById(admin._id).select('-password -refreshToken');
+
+const option = {
+    httpOnly: true,
+    secure: true,
+};
+
+return res
+    .status(200)
+    .cookie('accessToken', accessToken, option)
+    .cookie('refreshToken', refreshToken, option)
+    .json(
+        new apiResponse(
+            200,
+            {
+                admin: loggedAdmin,
+                accessToken,
+                refreshToken,
+            },
+            ' Admin Logged in successfully '
+        )
+    );
+
+//
+// refreshtoken rotation
+
+const refreshtokenController = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookie?.refreshToken || req.body?.refreshToken;
+
+    if (!incomingRefreshToken) {
+        throw new apiError(401, ' unauthorized request');
+    }
+
+    const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET);
+    if (!decodedToken) {
+        throw new apiError(401, ' invalid refresh token !');
+    }
+
+    const admin = await Admin.findById(decodedToken._id);
+
+    if (!admin) {
+        throw new apiError(401, ' invalid refresh token ');
+    }
+
+    if (incomingRefreshToken !== admin?.refreshToken) {
+        throw new apiError(401, ' Refresh token expired or invalid ');
+    }
+
+    const { accessToken, refreshToken } = await generateAccessTokenAndRefreshToken(admin._id);
+
+    const option = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .cookie('accessToken', accessToken, option)
+        .cookie('refreshToken', refreshToken, option)
+        .json(
+            new apiResponse(
+                200,
+                { accessToken, refreshToken },
+                ' accesstoken refreshed successfully '
+            )
+        );
+});
+
+// logout the admin
+
+const adminLogOut = asyncHandler(async (req, res) => {
+    await Admin.findByIdAndUpdate(
+        req.admin._id,
+        {
+            $unset: {
+                refreshToken: 1,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .clearCookie('accessToken', options)
+        .clearCookie('refreshToken', options)
+        .json(new apiResponse(200, {}, 'Admin logged out successfully'));
+});
+
+
+
+// for avatar 
+const avatarController = asyncHandler(async(req,res)=>{
+    const avatar = req.body
+    if (!avatar) {
+        throw new apiError(400, )
+        
+    }
+
+})
+
+
+
+export {adminLogin,  refreshtokenController,adminLogOut}
--- backend/src/models/admin.model.js
+++ backend/src/models/admin.model.js
@@ -6,67 +6,63 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const adminSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        phoneNumber: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        avatar: {
+            type: String,
+        },
+        coverImage: {
+            type: String,
+        },
+
+        refreshToken: {
+            type: String,
+        },
+        role: {
+            type: String,
+            required: true,
+        },
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    avatar: {
-      type: String,
-    },
-    coverImage: {
-      type: String,
-    },
-    role: {
-      type: String,
-    },
-    refreshToken: {
-      type: String,
-    },
-    role: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true },
+    { timestamps: true }
 );
 
-adminSchema.pre('save', async function () {
-  if (!this.isModified('password')) return;
-  this.password = await bcrypt.hash(this.password, 10);
+adminSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
 });
 
 adminSchema.methods.isPasswordCorrect = async function (password) {
-  return await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 };
 
 adminSchema.methods.generateAccessToken = function () {
-  return jsonwebtoken.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
-  });
+    return jsonwebtoken.sign({ _id: this._id }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    });
 };
 
 adminSchema.methods.generateRefreshToken = function () {
-  return jsonwebtoken.sign(
-    { _id: this._id },
-    process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
-  );
+    return jsonwebtoken.sign({ _id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    });
 };
 
 const Admin = mongoose.model('Admin', adminSchema);

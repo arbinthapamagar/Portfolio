17782767685da15cad9f6156a55138d70a7604e6
Arbeen <arbinbabuthapamagar2002@gmail.com>
2026-06-09 20:49:01 +0545

    Fix missing newline at end of project controller file

--- backend/src/controller/project.controller.js
+++ backend/src/controller/project.controller.js
@@ -4,6 +4,3 @@ import {apiError} from "../utils/apiError.js"
 import {apiResponse} from "../utils/apiResponse.js"
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
 
-const projectController = asyncHandler(async(req,res)=>{
-    const {title,description,problemSolved, stack,demoVideo, links}
-})
